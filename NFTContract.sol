08275ebe8e1d79fe13f48b9772693648448a7f6d
Mike Sepulveda <0xmsep@gmail.com>
2022-02-03 13:57:18 -0700

    test change

--- NFTContract.sol
+++ NFTContract.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 //import 1155 token contract from Openzeppelin
+
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
@@ -26,6 +27,7 @@ contract NFTContract is ERC1155, Ownable {
     function mint(address account, uint256 id, uint256 amount) public onlyOwner {
         _mint(account, id, amount, "");
     }
+    
     function burn(address account, uint256 id, uint256 amount) public {
         require(msg.sender == account);
         _burn(account, id, amount);
