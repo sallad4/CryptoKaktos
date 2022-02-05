05eb767af91a4bfef279352ceb189ba810b62e9e
Ryan Schwarting <schwartingryan@yahoo.com>
2022-02-05 10:56:21 -0700

    Create KaktosNFT.sol

---
+++ KaktosNFT.sol
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Kaktos is ERC721Enumerable, Ownable {
+  using Strings for uint256;
+
+  string public baseURI;
+  string public baseExtension = ".json";
+  string public notRevealedUri;
+  uint256 public cost = 1000000000000000000 wei;
+  uint256 public maxSupply = 1000;
+  uint256 public maxMintAmount = 10;
+  uint256 public nftPerAddressLimit = 5;
+  bool public paused = false;
+  bool public revealed = false;
+  bool public onlyWhitelisted = false;
+  address[] public whitelistedAddresses;
+  mapping(address => uint256) public addressesMintBalance;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initBaseURI,
+    string memory _initNotRevealedUri
+  ) ERC721(_name, _symbol) {
+    setBaseURI(_initBaseURI);
+    setNotRevealedURI(_initNotRevealedUri);
+  }
+
+  // internal
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+
+  // public
+  function mint(uint256 _mintAmount) public payable {
+    require(!paused, "The contract is paused");
+    uint256 supply = totalSupply();
+    require(_mintAmount > 0, "Need to atleast mint 1 NFT");
+    require(_mintAmount <= maxMintAmount, "Max amount per session exceeded");
+    require(supply + _mintAmount <= maxSupply, "Max NFT limit exceeded");
+
+    if (msg.sender != owner()) {
+        if (onlyWhitelisted == true) {
+            require(isWhitelisted(msg.sender), "User is not whitelisted");
+            uint256 ownerTokenCount = balanceOf(msg.sender);
+            require(ownerTokenCount < nftPerAddressLimit, "Max NFT per address exceeded");
+        }
+          require(msg.value >= cost * _mintAmount, "Insufficient funds");
+    }
+
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      _safeMint(msg.sender, supply + i);
+    }
+  }
+
+  function isWhitelisted(address _user) public view returns (bool) {
+      for(uint256 i = 0; i < whitelistedAddresses.length; i++) {
+          if (whitelistedAddresses[i] == _user) {
+              return true;
+          }
+      }
+      return false;
+  }
+
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+    
+    if(revealed == false) {
+        return notRevealedUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        : "";
+  }
+
+  //only owner
+  function reveal() public onlyOwner {
+      revealed = true;
+  }
+
+  function setNftperAddressLimit(uint256 _limit) public onlyOwner {
+    nftPerAddressLimit =_limit;
+  }
+  
+  function setCost(uint256 _newCost) public onlyOwner {
+    cost = _newCost;
+  }
+
+  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+    maxMintAmount = _newmaxMintAmount;
+  }
+  
+  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+    notRevealedUri = _notRevealedURI;
+  }
+
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
+
+  function pause(bool _state) public onlyOwner {
+    paused = _state;
+  }
+ 
+  function setOnlyWhitelisted(bool _state) public onlyOwner {
+    onlyWhitelisted = _state;
+  }
+
+ function whitelistUsers(address[] calldata _users) public onlyOwner {
+     delete whitelistedAddresses;
+     whitelistedAddresses =_users;
+  }
+
+  function withdraw() public payable onlyOwner { 
+    // This will payout the owner 100% of the contract balance.
+    // Do not remove this otherwise you will not be able to withdraw the funds.
+    // =============================================================================
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
+    // =============================================================================
+  }
+}
+
+/**
+The structure that the addresses need to be in when entering them for the whitelist
+ ["0xdD870fA1b7C4700F2BD7f44238821C26f7392148","0x583031D1113aD414F02576BD6afaBfb302140225",
+ "0x1aE0EA34a72D944a8C7603FfB3eC30a6669E454C","0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C"]
+ */
